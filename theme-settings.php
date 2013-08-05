5841710e5c3abd54ba2f19ff02545c53be885908
thewilkybarkid <chriswilkinson84@gmail.com>
2013-08-05 14:42:29 +0100

    Rename setting

--- theme-settings.php
+++ theme-settings.php
@@ -9,9 +9,9 @@ function cambridge_theme_form_system_theme_settings_alter(&$form, $form_state) {
     '#title' => t('Theme settings'),
     '#weight' => 0,
   );
-  $form['cambridge']['colour_theme'] = array(
+  $form['cambridge']['colour_scheme'] = array(
     '#type' => 'select',
-    '#title' => t('Colour'),
+    '#title' => t('Colour scheme'),
     '#options' => array(
       1 => 'Blue',
       2 => 'Turquoise',
@@ -21,7 +21,7 @@ function cambridge_theme_form_system_theme_settings_alter(&$form, $form_state) {
       6 => 'Red',
       7 => 'Gray',
     ),
-    '#default_value' => theme_get_setting('colour_theme'),
+    '#default_value' => theme_get_setting('colour_scheme'),
   );
 
   unset($form['logo']['default_logo']);
