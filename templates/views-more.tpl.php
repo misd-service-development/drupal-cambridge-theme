148ef3bfe074a8db6c8a5e973879af2641a631d5
thewilkybarkid <chriswilkinson84@gmail.com>
2013-09-13 09:56:41 +0100

    Add Views read more template

---
+++ templates/views-more.tpl.php
@@ -0,0 +1,7 @@
+<div class="campl-content-container campl-no-top-padding clearfix">
+  <p>
+    <a class="campl-secondary-cta campl-float-right" href="<?php print $more_url ?>">
+      <?php print $link_text; ?>
+    </a>
+  </p>
+</div>
