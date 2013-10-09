e08539ebeac2bcdb2ce15e324883fb1156b78142
thewilkybarkid <chriswilkinson84@gmail.com>
2013-10-09 14:03:11 +0100

    Make sure the sidebar appears if there's a carousel

--- templates/page.tpl.php
+++ templates/page.tpl.php
@@ -6,7 +6,7 @@ $has_carousel = isset($page['carousel']) && count($page['carousel']);
 $has_page_title = !$is_front && !$has_carousel && $title && $title != '';
 $has_left_navigation = isset($page['left_navigation']) && count($page['left_navigation']);
 $has_sub_content = isset($page['sub_content']) && count($page['sub_content']);
-$has_sidebar = isset($page['sidebar']) && count($page['sidebar']);
+$has_sidebar = (isset($page['sidebar']) && count($page['sidebar'])) || $has_carousel;
 $has_partnerships = isset($page['partnerships']) && count($page['partnerships']);
 
 ?>
