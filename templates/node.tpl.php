b833c0325049a2d3219a4c36c8e32c1a8c4052aa
JeebsUK <my.friend.of.misery@gmail.com>
2014-06-24 15:46:58 +0100

    Added new styles to the drupal.css file. Added new class full-node to the node.tpl.php. Added info to the changelog.

--- templates/node.tpl.php
+++ templates/node.tpl.php
@@ -1,4 +1,4 @@
-<div id="node-<?php print $node->nid; ?>" class="<?php print $classes; ?> clearfix"<?php print $attributes; ?>>
+<div id="node-<?php print $node->nid; ?>" class="<?php print $classes; ?> full-node clearfix"<?php print $attributes; ?>>
 
   <?php print $user_picture; ?>
 
